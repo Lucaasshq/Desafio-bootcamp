4dbdc0ff7cd3cd75f07eb311888b9adc3144e0ea
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-15 16:05:16 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -25,9 +23,35 @@ public class Main {
         mentoria1.setDescricao("Descricao mentoria java");
         mentoria1.setDate(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria1);
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Descricao bootcamp java Developer");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria1);
+
+
+        Dev devLucas = new Dev();
+
+        devLucas.setNome("Lucas");
+        devLucas.inscreverBootcamp(bootcamp1);
+
+
+        System.out.println("Conteudos inscritos Lucas " + devLucas.getConteudosIncritos());
+        
+        System.out.println("Conteudos concluidos Lucas " + devLucas.getConteudosConcluidos());
+        Dev devJoao = new Dev();
+
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteudos inscritos Joao " + devJoao.getConteudosIncritos());
+        System.out.println("Conteudos concluidos Joao " + devJoao.getConteudosConcluidos());
+
+
 
 
 
